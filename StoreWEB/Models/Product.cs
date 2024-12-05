654962fc20bda0173554b84e38bd8f3904949767
DevRufet <nesibovrufet47@gmail.com>
2024-12-05 17:29:33 +0400

    Add project files.

---
+++ StoreWEB/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace StoreWEB.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+
+        public bool Availability { get; set; }
+
+        public string Description { get; set; }
+
+
+        //relational
+        public int CategoryId { get; set; }
+        //public Category Category { get; set; } 
+
+    }
+}
